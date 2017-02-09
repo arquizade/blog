263a2ab591ad572759651d2bdd79abf8f022c87a
Dhenmark Arquiza <Dhenmark Arquiza>
2017-02-09 20:00:10 +0800

    update_view create

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,17 @@
+@extends('main')
+@section('title', '| Create New Post')
+@section('stylesheet')
+<!-- This area is for CSS -->
+@endsection
+@section('content')
+<div class="row">
+	<div class="col-md-8 col-md-offset-2">
+		<h1>Create New Post</h1>
+		<hr>
+		
+	</div>
+</div>
+@endsection
+@section('script')
+<!-- This area is for Java Scripting -->
+@endsection
